abfc58395c4191afaadf9f76a4a2d23907e961c7
Abhijith C S <Abhijith.cs@ad.infosys.com>
2017-01-18 01:49:06 +0530

    Service Integrations - Profile Page

--- www/js/account/account.controllers.js
+++ www/js/account/account.controllers.js
@@ -1,38 +1,32 @@
 angular.module('zaitoonFirst.account.controllers', [])
 
-.controller('ProfileCtrl', function($scope, $http, $ionicPopover, $ionicPopup, $ionicActionSheet, $state) {
+.controller('ProfileCtrl', function($scope, $rootScope, $http, user, ProfileService, $ionicPopover, $ionicPopup, $ionicActionSheet, $state) {
   
+  //Settings
+  $scope.show_new_address_button = false;   //Don't give a provision to add new address here.
+
+  $scope.customer = user; 
+
   $scope.data = {};
   $scope.addressCount = 0;
 
-  $http.get('http://localhost/vega-web-app/online/fetchusers.php')
-  .then(function(response){
-        $scope.customer = response.data; 
-        $scope.user_shipping_addresses = $scope.customer.savedAddresses;
-        $scope.addressCount = $scope.customer.savedAddresses.length;
+  $scope.user_shipping_addresses = $scope.customer.savedAddresses;
+  $scope.addressCount = $scope.customer.savedAddresses.length;
 
-        //Set the default address
-        var i = 0;
-        while(i < $scope.user_shipping_addresses.length){
-          if($scope.user_shipping_addresses[i].isDefault){
-            $scope.data.selected_address = $scope.user_shipping_addresses[0];
-            break;
-          }
-          i++;
-        }
 
-        //Show ADD NEW if empty
-        if($scope.user_shipping_addresses.length == 0)
-          $scope.show_new_address_button = true;
-        else
-          $scope.show_new_address_button = false;
-        
-  });
+  //Set the default address
+    var i = 0;
+    while(i < $scope.user_shipping_addresses.length){
+      if($scope.user_shipping_addresses[i].isDefault){
+        $scope.data.selected_address = $scope.user_shipping_addresses[i];
+        break;
+      }
+      i++;
+    }
 
-                
+       
 
-  
-
+          
   //Edit Profile
   $scope.isEditMode = false;
 
@@ -61,8 +55,10 @@ angular.module('zaitoonFirst.account.controllers', [])
   $scope.saveEdit = function(){
     $scope.isEditMode = false;
     document.getElementById("inputProfileName").style.borderBottom="1px dashed #bdc3c7";
-    document.getElementById("inputProfileEmail").style.borderBottom="1px dashed #bdc3c7";    
+    document.getElementById("inputProfileEmail").style.borderBottom="1px dashed #bdc3c7"; 
+
     //Call http request and make the changes in the servers
+    ProfileService.updateUserData($scope.customer.name, $scope.customer.email);
   }  
 
 
@@ -79,7 +75,7 @@ angular.module('zaitoonFirst.account.controllers', [])
 		$scope.addresses_popover.hide();
 	};
 
-
+  
   $scope.logout = function(){
     $ionicActionSheet.show({
       titleText: 'Are you sure you want to logout?',
@@ -100,16 +96,44 @@ angular.module('zaitoonFirst.account.controllers', [])
     var editAddressPopup = $ionicPopup.show({
       cssClass: 'popup-outer edit-shipping-address-view',
       templateUrl: 'views/checkout/partials/edit-shipping-address-popup.html',
-      title: address.street,
+      title: address.name,
       scope: $scope,
       buttons: [
-        { text: 'Close' },
+        { text: 'Cancel' },
         {
-          text: 'Delete',
-					// type: 'icon-left ion-trash-a delete-button',
+          text: 'Delete',					
 					type: 'delete-button',
           onTap: function(e) {
-            // return $scope.data;
+            var response = ProfileService.deleteSavedAddress(address.id);
+            if(response){
+              //Successfully deleted. Hide from current list of addresses.
+              var i = 0;
+              while(i < $scope.user_shipping_addresses.length){
+                if(address.id == $scope.user_shipping_addresses[i].id){
+                  $scope.user_shipping_addresses.splice(i, 1);                                    
+                  $scope.addresses_popover.hide();   
+
+                  $scope.data.selected_address = "";
+
+                  if($scope.user_shipping_addresses.length == 0)
+                    $state.reload();                  
+ 
+                  //Set the default address
+                  var i = 0;
+                  while(i < $scope.user_shipping_addresses.length){
+                    if($scope.user_shipping_addresses[i].isDefault){
+                      $scope.data.selected_address = $scope.user_shipping_addresses[i];
+                      break;
+                    }
+                    i++;
+                  } 
+
+                  break;
+                }                
+                i++;                
+              }
+
+            }
           }
         },
         {
@@ -134,10 +158,7 @@ angular.module('zaitoonFirst.account.controllers', [])
 
   $http.get('http://localhost/vega-web-app/online/orderhistory.php?id=0')
   .then(function(response){
-        console.log('********* am here');
-        console.log(response.data);
         $scope.orders = response.data;
-        console.log($scope.menu);
         $scope.left = 1;
     });
   $scope.limiter=5;
--- www/js/account/account.services.js
+++ www/js/account/account.services.js
@@ -41,11 +41,30 @@ angular.module('zaitoonFirst.account.services', [])
 .service('ProfileService', function ($http, $q){
   this.getUserData = function(){
     var dfd = $q.defer();
-    $http.get('logged_user_db.json').success(function(database) {
-      dfd.resolve(database.user);
+    $http.get('http://localhost/vega-web-app/online/fetchusers.php').success(function(data) {
+      if(data.savedAddresses == null)
+        data.savedAddresses=[];
+      dfd.resolve(data);
     });
     return dfd.promise;
   };
+
+
+  this.updateUserData = function(newName, newEmail){
+    $http.get('http://localhost/vega-web-app/online/updateuser.php?name='+newName+'&email='+newEmail+'&id=9043960876').success(function(response) {
+      
+    });
+  };
+
+  //Delete a saved Address.
+  this.deleteSavedAddress = function(id){
+    $http.get('http://localhost/vega-web-app/online/deletesavedaddress.php?id='+id+'&user=9043960876').success(function(response) {
+      
+    });
+    return true;
+  };  
+
+
 })
 
 ;
