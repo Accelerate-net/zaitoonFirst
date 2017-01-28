f813a61300578957bc6fe1460320f41698a35deb
Abhijith C S <Abhijith.cs@ad.infosys.com>
2017-01-28 22:58:50 +0530

    Service Integrated to all cuisines

--- www/js/feed/feed.controllers.js
+++ www/js/feed/feed.controllers.js
@@ -171,66 +171,351 @@ angular.module('zaitoonFirst.feed.controllers', [])
 
 })
 
-.controller('FoodChineseCtrl', function($scope, products, ShoppingCartService, $ionicLoading) {
-	$scope.products = products;
+.controller('FoodChineseCtrl', function($scope, $rootScope, $http, ShoppingCartService, $ionicLoading, $ionicPopup) {
+	
+	var custom_filter = !_.isUndefined(window.localStorage.customFilter) ? window.localStorage.customFilter : [];
+	
+	//To display things if filter is applied
+	if(custom_filter.length > 0) 
+		$scope.isFilter = true;
+	else
+		$scope.isFilter = false;
+
+
+	//Receiving Broadcast - If Filter Applied
+	$rootScope.$on('filter_applied', function(event, filter) {
+		window.localStorage.customFilter = JSON.stringify(filter);
+    	$scope.reinitializeMenu();
+  	});
+
+  	$scope.clearFilter = function(){
+		$scope.isFilter = false;
+		window.localStorage.customFilter = "";
+		custom_filter = [];
+		$scope.reinitializeMenu();
+	}
+
+
+	// Making request to server to fetch-menu
+	var init = $scope.reinitializeMenu = function(){
+        var data = {}; 
+        data.cuisine = "CHINESE";
+        data.isFilter = false;
+
+        if(custom_filter.length > 0){
+        	data.isFilter = true;
+        	data.filter = custom_filter;     
+        	console.log(data.filter);   	
+    	}
+
+        $http({
+          method  : 'POST',
+          url     : 'http://localhost/vega-web-app/online/fetchmenu.php',
+          data    : data, //forms user object
+          headers : {'Content-Type': 'application/x-www-form-urlencoded'} 
+         })
+        .then(function(response) {
+			$scope.menu = response.data;    
+        });  
+    }
+
+    init();
+
+
+    //For Search field
+	$scope.search = { query : '' };
+	$scope.showSearch = false;
+
+
+	$scope.resetSearch = function(){
+		$scope.search = { query : '' };
+		$scope.showSearch = !$scope.showSearch;
+	}
+
+
+	  $scope.customOptions = function(product) {
+
+	  	//Render Template
+	  	var i = 0;
+	  	$scope.choiceName = "";
+	  	$scope.choicePrice = "";
+	  	var choiceTemplate = '<div class="padding">';
+	  	while(i < product.custom.length){
+	  		choiceTemplate = choiceTemplate + '<ion-radio ng-click="addCustomItem(\''+product.custom[i].customName+'\', '+product.custom[i].customPrice+')">'+product.custom[i].customName+' <tag style="font-size: 80%; color: #d35400">Rs. '+product.custom[i].customPrice+'</tag></ion-radio>';
+	  		i++;
+	  	}
+	  	choiceTemplate = choiceTemplate + '</div>';
+
+	    var newCustomPopup = $ionicPopup.show({
+	      cssClass: 'popup-outer new-shipping-address-view',
+	      template: choiceTemplate,
+	      title: 'Choose Options',
+	      scope: $scope,
+	      buttons: [
+	        { text: 'Cancel' }      
+	      ]
+	    });
+	    $scope.addCustomItem = function(variant, price){
+	    	$scope.choiceName = variant;
+	  		$scope.choicePrice = price;
+            
+            if($scope.choiceName != "" && $scope.choicePrice != ""){
+            	product.itemPrice = $scope.choicePrice;
+            	product.variant = $scope.choiceName;
+
+            	$scope.addToCart(product);
+            	newCustomPopup.close(); 
+            }  	  		
+	  		
+	    }
+
+	  }
+
+
 
 	$scope.addToCart = function(product) {
 		$ionicLoading.show({
-			template:  '<b style="color: #f1c40f">'+product.name+'</b> is added.',
+			template:  '<b style="color: #f1c40f">'+product.itemName+'</b> is added.',
 			duration: 1000
 		});
 
 		product.qty = 1;
-		product.size = "M";
-		product.color = "black";
-		product.price = 150;
   	    ShoppingCartService.addProduct(product);
-  	    //console.log(product);
-  };
+  	};
 
 
 })
 
-.controller('FoodIndianCtrl', function($scope, products, ShoppingCartService, $ionicLoading) {
-	$scope.products = products;
+
+.controller('FoodIndianCtrl', function($scope, $rootScope, $http, ShoppingCartService, $ionicLoading, $ionicPopup) {
+	
+	var custom_filter = !_.isUndefined(window.localStorage.customFilter) ? window.localStorage.customFilter : [];
+	
+	//To display things if filter is applied
+	if(custom_filter.length > 0) 
+		$scope.isFilter = true;
+	else
+		$scope.isFilter = false;
+
+
+	//Receiving Broadcast - If Filter Applied
+	$rootScope.$on('filter_applied', function(event, filter) {
+		window.localStorage.customFilter = JSON.stringify(filter);
+    	$scope.reinitializeMenu();
+  	});
+
+  	$scope.clearFilter = function(){
+		$scope.isFilter = false;
+		window.localStorage.customFilter = "";
+		custom_filter = [];
+		$scope.reinitializeMenu();
+	}
+
+
+	// Making request to server to fetch-menu
+	var init = $scope.reinitializeMenu = function(){
+        var data = {}; 
+        data.cuisine = "INDIAN";
+        data.isFilter = false;
+
+        if(custom_filter.length > 0){
+        	data.isFilter = true;
+        	data.filter = custom_filter;     
+        	console.log(data.filter);   	
+    	}
+
+        $http({
+          method  : 'POST',
+          url     : 'http://localhost/vega-web-app/online/fetchmenu.php',
+          data    : data, //forms user object
+          headers : {'Content-Type': 'application/x-www-form-urlencoded'} 
+         })
+        .then(function(response) {
+			$scope.menu = response.data;    
+        });  
+    }
+
+    init();
+
+
+    //For Search field
+	$scope.search = { query : '' };
+	$scope.showSearch = false;
+
+
+	$scope.resetSearch = function(){
+		$scope.search = { query : '' };
+		$scope.showSearch = !$scope.showSearch;
+	}
+
+
+	  $scope.customOptions = function(product) {
+
+	  	//Render Template
+	  	var i = 0;
+	  	$scope.choiceName = "";
+	  	$scope.choicePrice = "";
+	  	var choiceTemplate = '<div class="padding">';
+	  	while(i < product.custom.length){
+	  		choiceTemplate = choiceTemplate + '<ion-radio ng-click="addCustomItem(\''+product.custom[i].customName+'\', '+product.custom[i].customPrice+')">'+product.custom[i].customName+' <tag style="font-size: 80%; color: #d35400">Rs. '+product.custom[i].customPrice+'</tag></ion-radio>';
+	  		i++;
+	  	}
+	  	choiceTemplate = choiceTemplate + '</div>';
+
+	    var newCustomPopup = $ionicPopup.show({
+	      cssClass: 'popup-outer new-shipping-address-view',
+	      template: choiceTemplate,
+	      title: 'Choose Options',
+	      scope: $scope,
+	      buttons: [
+	        { text: 'Cancel' }      
+	      ]
+	    });
+	    $scope.addCustomItem = function(variant, price){
+	    	$scope.choiceName = variant;
+	  		$scope.choicePrice = price;
+            
+            if($scope.choiceName != "" && $scope.choicePrice != ""){
+            	product.itemPrice = $scope.choicePrice;
+            	product.variant = $scope.choiceName;
+
+            	$scope.addToCart(product);
+            	newCustomPopup.close(); 
+            }  	  		
+	  		
+	    }
+
+	  }
+
+
 
 	$scope.addToCart = function(product) {
 		$ionicLoading.show({
-			template:  '<b style="color: #f1c40f">'+product.name+'</b> is added.',
+			template:  '<b style="color: #f1c40f">'+product.itemName+'</b> is added.',
 			duration: 1000
 		});
 
 		product.qty = 1;
-		product.size = "M";
-		product.color = "black";
-		product.price = 150;
   	    ShoppingCartService.addProduct(product);
-  	    //console.log(product);
-  };
+  	};
 
 
 })
 
-.controller('FoodDessertCtrl', function($scope, products, ShoppingCartService, $ionicLoading) {
-	$scope.products = products;
+.controller('FoodDessertCtrl', function($scope, $rootScope, $http, ShoppingCartService, $ionicLoading, $ionicPopup) {
+	
+	var custom_filter = !_.isUndefined(window.localStorage.customFilter) ? window.localStorage.customFilter : [];
+	
+	//To display things if filter is applied
+	if(custom_filter.length > 0) 
+		$scope.isFilter = true;
+	else
+		$scope.isFilter = false;
+
+
+	//Receiving Broadcast - If Filter Applied
+	$rootScope.$on('filter_applied', function(event, filter) {
+		window.localStorage.customFilter = JSON.stringify(filter);
+    	$scope.reinitializeMenu();
+  	});
+
+  	$scope.clearFilter = function(){
+		$scope.isFilter = false;
+		window.localStorage.customFilter = "";
+		custom_filter = [];
+		$scope.reinitializeMenu();
+	}
+
+
+	// Making request to server to fetch-menu
+	var init = $scope.reinitializeMenu = function(){
+        var data = {}; 
+        data.cuisine = "DESSERTS";
+        data.isFilter = false;
+
+        if(custom_filter.length > 0){
+        	data.isFilter = true;
+        	data.filter = custom_filter;     
+        	console.log(data.filter);   	
+    	}
+
+        $http({
+          method  : 'POST',
+          url     : 'http://localhost/vega-web-app/online/fetchmenu.php',
+          data    : data, //forms user object
+          headers : {'Content-Type': 'application/x-www-form-urlencoded'} 
+         })
+        .then(function(response) {
+			$scope.menu = response.data;    
+        });  
+    }
+
+    init();
+
+
+    //For Search field
+	$scope.search = { query : '' };
+	$scope.showSearch = false;
+
+
+	$scope.resetSearch = function(){
+		$scope.search = { query : '' };
+		$scope.showSearch = !$scope.showSearch;
+	}
+
+
+	  $scope.customOptions = function(product) {
+
+	  	//Render Template
+	  	var i = 0;
+	  	$scope.choiceName = "";
+	  	$scope.choicePrice = "";
+	  	var choiceTemplate = '<div class="padding">';
+	  	while(i < product.custom.length){
+	  		choiceTemplate = choiceTemplate + '<ion-radio ng-click="addCustomItem(\''+product.custom[i].customName+'\', '+product.custom[i].customPrice+')">'+product.custom[i].customName+' <tag style="font-size: 80%; color: #d35400">Rs. '+product.custom[i].customPrice+'</tag></ion-radio>';
+	  		i++;
+	  	}
+	  	choiceTemplate = choiceTemplate + '</div>';
+
+	    var newCustomPopup = $ionicPopup.show({
+	      cssClass: 'popup-outer new-shipping-address-view',
+	      template: choiceTemplate,
+	      title: 'Choose Options',
+	      scope: $scope,
+	      buttons: [
+	        { text: 'Cancel' }      
+	      ]
+	    });
+	    $scope.addCustomItem = function(variant, price){
+	    	$scope.choiceName = variant;
+	  		$scope.choicePrice = price;
+            
+            if($scope.choiceName != "" && $scope.choicePrice != ""){
+            	product.itemPrice = $scope.choicePrice;
+            	product.variant = $scope.choiceName;
+
+            	$scope.addToCart(product);
+            	newCustomPopup.close(); 
+            }  	  		
+	  		
+	    }
+
+	  }
+
+
 
 	$scope.addToCart = function(product) {
 		$ionicLoading.show({
-			template:  '<b style="color: #f1c40f">'+product.name+'</b> is added.',
+			template:  '<b style="color: #f1c40f">'+product.itemName+'</b> is added.',
 			duration: 1000
 		});
 
 		product.qty = 1;
-		product.size = "M";
-		product.color = "black";
-		product.price = 150;
   	    ShoppingCartService.addProduct(product);
-  	    //console.log(product);
-  };
+  	};
 
 
 })
-
 
 
 
